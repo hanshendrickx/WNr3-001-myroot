93817bfc86d5c263a358b3ed2d58f6c90ce4d3a2
Hans <hhlhendrickx@gmail.com>
2022-03-11 09:53:24 +0100

    Basic Start MP50
    Django 3.2. and wagtail 2.16.1 MP50

---
+++ MP50/MP50/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^tj&vb^t55s^f4$n1*q887f1@iqj*-3v8q%5#x*3jr+p4y(xxu'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
